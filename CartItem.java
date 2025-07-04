6b04c90bab643c4841b7ccf6beaa2465f528a32c
Ahmed <as3992054@gmail.com>
2025-07-04 15:03:22 +0300

    Add Customer and some edit in cart item

--- CartItem.java
+++ CartItem.java
@@ -2,23 +2,30 @@ public class CartItem {
     private Product product;
     private int quantity;
 
-    public CartItem(Product product, int quantity)
-    {
-        if(quantity <= 0)
-        {
-            throw new IllegalArgumentException("Quantity must be greater than 0.");
+    public CartItem(Product product, int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity in cart item must be greater than 0.");
         }
         this.product = product;
         this.quantity = quantity;
     }
 
-    public  Product getProduct()
-    {
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public void setQuantity(int quantity) {
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity in cart item must be greater than 0.");
+        }
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
         return product;
     }
 
-    public  int getQuantity()
-    {
+    public int getQuantity() {
         return quantity;
     }
 }
--- Customer.java
+++ Customer.java
@@ -1,2 +1,31 @@
 public class Customer {
+    private String name;
+    private double balance;
+
+    public Customer(String name, double balance) {
+        if(balance < 0.0) {
+            throw new IllegalArgumentException("Customer balance must be greater than or equal 0.");
+        }
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBalance(double balance) {
+        if(balance < 0.0) {
+            throw new IllegalArgumentException("Customer balance must be greater than or equal 0.");
+        }
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
