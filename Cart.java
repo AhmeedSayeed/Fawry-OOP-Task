4ff236e1aaace49fa6226bbc70d51be9e4a72d9f
Ahmed <as3992054@gmail.com>
2025-07-04 16:39:49 +0300

    Add Cart and some getters/setters in product

--- Cart.java
+++ Cart.java
@@ -1,2 +1,75 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Cart {
+    private ArrayList<CartItem> cartItems = new ArrayList<>();
+
+    public void add(Product product, int quantity) {
+        if (quantity > product.getQuantity()) {
+            throw new IllegalArgumentException("Requested quantity exceeds available stock.");
+        }
+        for (CartItem cartItem : cartItems) {
+            if(cartItem.getProduct().equals(product)) {
+                int newQuantity = cartItem.getQuantity() + quantity;
+                if (newQuantity > product.getQuantity()) {
+                    throw new IllegalArgumentException("Requested quantity exceeds available stock.");
+                }
+                cartItem.setQuantity(cartItem.getQuantity() + quantity);
+                return;
+            }
+        }
+        cartItems.add(new CartItem(product, quantity));
+    }
+
+    public void remove(Product product) {
+        cartItems.removeIf(cartItem -> cartItem.getProduct().equals(product));
+    }
+
+    // Increase product quantity with some value (quantity parameter).
+    public void increaseQuantity(Product product, int quantity) {
+        for (CartItem cartItem : cartItems) {
+            if (cartItem.getProduct().equals(product)) {
+                int newQuantity = cartItem.getQuantity() + quantity;
+                if (newQuantity > product.getQuantity()) {
+                    throw new IllegalArgumentException("Requested quantity exceeds available stock.");
+                }
+                cartItem.setQuantity(newQuantity);
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Product not found in cart.");
+    }
+
+    // Decrease product quantity with some value (quantity parameter) and remove it safely by iterator if reached zero,
+    // throw error if negative
+    public void decreaseQuantity(Product product, int quantity) {
+        Iterator<CartItem> iterator = cartItems.iterator();
+        while (iterator.hasNext()) {
+            CartItem cartItem = iterator.next();
+            if (cartItem.getProduct().equals(product)) {
+                int currentQuantity = cartItem.getQuantity();
+                int newQuantity = currentQuantity - quantity;
+                if (newQuantity > 0) {
+                    cartItem.setQuantity(newQuantity);
+                }
+                else if (newQuantity == 0) {
+                    iterator.remove();
+                }
+                else {
+                    throw new IllegalArgumentException("Cannot decrease more than the current quantity.");
+                }
+                return;
+            }
+        }
+        throw new IllegalArgumentException("Product not found in cart.");
+    }
+
+
+    public boolean isEmpty() {
+        return cartItems.isEmpty();
+    }
+
+    public ArrayList<CartItem> getCartItems() {
+        return cartItems;
+    }
 }
--- Product.java
+++ Product.java
@@ -27,6 +27,26 @@ public class Product implements Shippable {
         return expired.orElse(false);
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
     @Override
     public String getName() {
         return name;
