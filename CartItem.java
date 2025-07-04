b97a65a53d22a1098b84c680473428a948b533c8
Ahmed <as3992054@gmail.com>
2025-07-04 14:58:17 +0300

    Add Cart Item

--- CartItem.java
+++ CartItem.java
@@ -1,2 +1,24 @@
 public class CartItem {
+    private Product product;
+    private int quantity;
+
+    public CartItem(Product product, int quantity)
+    {
+        if(quantity <= 0)
+        {
+            throw new IllegalArgumentException("Quantity must be greater than 0.");
+        }
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public  Product getProduct()
+    {
+        return product;
+    }
+
+    public  int getQuantity()
+    {
+        return quantity;
+    }
 }
